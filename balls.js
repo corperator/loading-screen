2b935b441d027295c86409b3cede1954c6b64f2f
corperator <159652588+corperator@users.noreply.github.com>
2024-02-10 17:07:17 -0300

    Update balls.js

--- balls.js
+++ balls.js
@@ -1,1 +1,12 @@
-alert('Hello")
+window.onload = setInterval(GameLoop, 1000 / 10); //10fps
+
+let myAudio = new Audio();
+
+myAudio.src = '/audio/indaclub.ogg';
+
+function Gameloop() {
+    myAudio.play();
+    if (myAudio.paused == true) {
+        myAudio.play();
+    }
+}
